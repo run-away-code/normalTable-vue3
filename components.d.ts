b5787abafd1f48f751c2f5add5e1dd5c1dab5002
yangbo <yangbo@deltaphone.com>
2024-05-23 16:05:50 +0800

    featv1.3.2

--- components.d.ts
+++ components.d.ts
@@ -7,19 +7,15 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElCheckboxGroup: typeof import('element-plus/es')['ElCheckboxGroup']
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElInputNumber: typeof import('element-plus/es')['ElInputNumber']
-    ElMain: typeof import('element-plus/es')['ElMain']
     ElPagination: typeof import('element-plus/es')['ElPagination']
     ElRadio: typeof import('element-plus/es')['ElRadio']
     ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
-    ElTable: typeof import('element-plus/es')['ElTable']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
-    'Index_副本': typeof import('./src/components/NormalInput/index_副本.vue')['default']
     InputNumber: typeof import('./src/components/NormalInput/inputNumber.vue')['default']
     NormalCheckbox: typeof import('./src/components/NormalCheckbox/index.vue')['default']
     NormalDate: typeof import('./src/components/NormalDate/index.vue')['default']
